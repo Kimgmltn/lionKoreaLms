f01452cf41ec2ebb301a6d6fb2872504ab9abdf0
Kimgmltn <Kimgmltn0323@gmail.com>
2024-08-19 00:12:12 +0900

    spring security 설정. 암호화 빈까지 생성

---
+++ src/main/java/kr/co/lionkorea/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package kr.co.lionkorea.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/login","/js/**", "/css/**", "/img/**").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin((formLogin -> formLogin.loginPage("/login").permitAll()))
+                .csrf(AbstractHttpConfigurer::disable)
+                .rememberMe(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
