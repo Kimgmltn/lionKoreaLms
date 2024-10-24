cd35cc206e5bd239842a997eaeb5a54dd4bb7325
Kimgmltn <Kimgmltn0323@gmail.com>
2024-10-25 00:20:20 +0900

    springSecurity에 permit api 추가

--- src/main/java/kr/co/lionkorea/config/SecurityConfig.java
+++ src/main/java/kr/co/lionkorea/config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
         loginFilter.setFilterProcessesUrl("/api/auth/login");
         http    // 권한별 API 접근 설정
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/api/auth/login", "/api/auth/reissue").permitAll()
+                        .requestMatchers("/api/auth/login", "/api/auth/reissue", "/api/members/password/*").permitAll()
                         .requestMatchers("/api/members/**").hasAnyRole("ADMIN", "SUPER_ADMIN")
                         .requestMatchers("/api/**").authenticated()
 //                        .requestMatchers("/login","/js/**", "/css/**", "/img/**", "/h2-console/**", "/").permitAll()
