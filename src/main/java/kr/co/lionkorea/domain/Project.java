3688d3569b1f6b6321cc147a8dc7f059a70f4d02
khs <T1900843@partner.tmapmobility.com>
2024-11-14 21:13:12 +0900

    Project 엔티티 생성

--- src/main/java/kr/co/lionkorea/domain/Project.java
+++ src/main/java/kr/co/lionkorea/domain/Project.java
@@ -21,5 +21,28 @@ public class Project extends BaseEntity{
     private Long id;
     @Column(name = "project_name")
     private String projectName;
+    @Column(name = "language")
+    private String language;
+    @Column(name = "process_status")
+    private String processStatus;
+    @Column(name = "consultation_date")
+    private String consultationDate;
+    @Column(name = "time_period")
+    private String timePeriod;
+    @Column(name = "hour")
+    private Integer hour;
+    @Column(name = "minute")
+    private Integer minute;
+    @Column(name = "consultation_notes")
+    private String consultationNotes;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "translator_id", referencedColumnName = "member_id")
+    private Member translator;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "buyer_id", referencedColumnName = "company_id")
+    private Buyer buyer;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "domestic_id", referencedColumnName = "company_id")
+    private DomesticCompany domesticCompany;
 }
