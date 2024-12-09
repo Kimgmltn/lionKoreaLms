edd280ae24c7cc70ea5977784e130835142ab045
Kimgmltn <Kimgmltn0323@gmail.com>
2024-12-10 03:27:19 +0900

    js에서 logout api 경로 변경

--- src/main/resources/static/js/logout.js
+++ src/main/resources/static/js/logout.js
@@ -3,7 +3,7 @@ import {post} from './api.js'
 document.getElementById('logout-image').addEventListener('click', async (event) => {
     event.preventDefault();
 
-    const response = await post('/api/auth/logout');
+    const response = await post('/logout');
     sessionStorage.clear();
     window.location.href = '/login';
 })
